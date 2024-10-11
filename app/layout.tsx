b09537e32a6b339513680783c0cda9f7ed5819ef
sina <cna_fp@yahoo.com>
2024-10-11 12:15:36 +0330

    add: nextjs Analatics package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import { MainLayout } from '@components';
 import { inter } from '@constants';
-
 import './globals.css';
 
 export default function RootLayout({
@@ -12,6 +13,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <MainLayout>{children}</MainLayout>
+        <Analytics />
       </body>
     </html>
   );
