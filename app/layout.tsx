e466c5e471030f658adaf09413d26f095e0be2d6
sina <cna_fp@yahoo.com>
2024-10-14 17:09:26 +0330

    add: Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 import { MainLayout } from '@components';
 import { inter } from '@constants';
+
 import './globals.css';
 
 export default function RootLayout({
@@ -14,6 +16,15 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         <MainLayout>{children}</MainLayout>
         <Analytics />
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "oii4kppcr6");
+          `}
+        </Script>
       </body>
     </html>
   );
